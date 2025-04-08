db3b32b4df9b85d970b12c8a96f9f89d248d1c25
Olatunji Ruwase <olruwase@microsoft.com>
2025-04-08 07:22:42 -0400

    Cleanup scripts

---
+++ inference/sglang/run_llama3_8B.sh
@@ -0,0 +1,9 @@
+export LOCAL_RANK=0
+DATASET_OPTS="--dataset-name random --random-input-len 512 --random-output-len 32 --random-range-ratio 1.0"
+BATCH_SIZE=128
+MODEL_NAME="meta-llama/Meta-Llama-3.1-8B-Instruct"
+
+python -m sglang.bench_offline_throughput --model-path ${MODEL_NAME}  ${DATASET_OPTS} --num-prompts ${BATCH_SIZE} --disable-cuda-graph --zero-inference-config ds_offload_cpu.json
+python -m sglang.bench_offline_throughput --model-path ${MODEL_NAME}  ${DATASET_OPTS} --num-prompts ${BATCH_SIZE} --disable-cuda-graph --zero-inference-config ds_offload_nvme_aio.json
+python -m sglang.bench_offline_throughput --model-path ${MODEL_NAME}  ${DATASET_OPTS} --num-prompts ${BATCH_SIZE} --disable-cuda-graph --zero-inference-config ds_offload_nvme_gds.json
+python -m sglang.bench_offline_throughput --model-path ${MODEL_NAME}  ${DATASET_OPTS} --num-prompts ${BATCH_SIZE} --disable-cuda-graph
