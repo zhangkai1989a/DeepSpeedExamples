b018de149e819c7adc58e705899d328b06c5d310
Hongwei Chen <33092912+hwchen2017@users.noreply.github.com>
2025-06-11 22:21:32 -0700

    Update domino example (#976)
    * remove files
    
    Signed-off-by: Hongwei Chen <hongweichen@microsoft.com>
    
    * Update domino example
    
    Signed-off-by: Hongwei Chen <hongweichen@microsoft.com>
    
    * apply review suggestions
    
    Signed-off-by: Hongwei Chen <hongweichen@microsoft.com>
    
    ---------
    
    Signed-off-by: Hongwei Chen <hongweichen@microsoft.com>

--- training/DeepSpeed-Domino/pretrain_gpt.py
+++ training/DeepSpeed-Domino/pretrain_gpt.py
@@ -1,124 +1,46 @@
 # Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
 # This file is adapted from pretrain_gpt.py in Megatron-LM
 
-import time
+from functools import partial
+
 import torch
-from domino.utils import print_rank_0
-from domino.initialize import initialize_domino, set_jit_fusion_options
-from domino.arguments import get_args, core_transformer_config_from_args
-from domino.data.gpt_dataset import build_train_valid_test_datasets
+
+from megatron import get_args
+from megatron import get_timers
+from megatron import get_tokenizer
+from megatron import print_rank_0
+from megatron.arguments import core_transformer_config_from_args
+from megatron.core import tensor_parallel
+from megatron.data.gpt_dataset import build_train_valid_test_datasets
+from megatron.utils import get_ltor_masks_and_position_ids
+from megatron.utils import average_losses_across_data_parallel_group
+
+from domino.gpt_model import GPTModel
 from domino.training import pretrain
-from domino.modules.module import DominoModule
-from domino.modules.enums import AttnMaskType
-from domino.language_model import parallel_lm_logits
-from domino.language_model import get_language_model
-from domino.tensor_parallel.cross_entropy import vocab_parallel_cross_entropy
 
+def is_rank_0():
+    # if torch.cuda.current_device() == 0:
+    if torch.distributed.get_rank() == 0:
+        return True
 
-_TRAIN_START_TIME = time.time()
-
-def post_language_model_processing(lm_output, labels, logit_weights, parallel_output):
-    output = parallel_lm_logits(lm_output, logit_weights, parallel_output)
-    labels = labels.transpose(0, 1).contiguous()
-    loss = vocab_parallel_cross_entropy(output.float(), labels)
-    loss = loss.transpose(0, 1).contiguous()
-    return loss
-
-
-class GPTModel(DominoModule):
-    def __init__(
-        self,
-        config,
-        num_tokentypes=0,
-        parallel_output=True,
-        pre_process=True,
-        post_process=True,
-    ):
-        super().__init__(config=config)
-        self.parallel_output = parallel_output
-        self.pre_process = pre_process
-        self.post_process = post_process
-        self.language_model = get_language_model(
-            config=config,
-            num_tokentypes=num_tokentypes,
-            encoder_attn_mask_type=AttnMaskType.causal,
-            pre_process=self.pre_process,
-            post_process=self.post_process,
-        )
-        self.initialize_word_embeddings()
-
-    def set_input_tensor(self, input_tensor):
-        self.language_model.set_input_tensor(input_tensor)
-
-    def forward(
-        self,
-        input_ids,
-        position_ids,
-        attention_mask,
-        labels=None,
-        inference_params=None,
-    ):
-        lm_output = self.language_model(
-            input_ids,
-            position_ids,
-            attention_mask,
-            inference_params=inference_params,
-        )
-
-        if self.post_process:
-            return post_language_model_processing(
-                lm_output,
-                labels,
-                self.shared_embedding_or_output_weight(),
-                self.parallel_output,
-            )
-        else:
-            return lm_output
-
-def main():
-    initialize_domino()
-
-    # Set pytorch JIT layer fusion options and warmup JIT functions.
-    set_jit_fusion_options()
-
-    # Adjust the startup time so it reflects the largest value.
-    # This will be closer to what scheduler will see (outside of
-    # image ... launches.
-    global _TRAIN_START_TIME
-    start_time_tensor = torch.cuda.DoubleTensor([_TRAIN_START_TIME])
-    torch.distributed.all_reduce(start_time_tensor,
-                                 op=torch.distributed.ReduceOp.MIN)
-    _TRAIN_START_TIME = start_time_tensor.item()
-    print_rank_0('time to initialize megatron (seconds): {:.3f}'.format(
-        time.time() - _TRAIN_START_TIME))
-
+def model_builder(pre_process=True, post_process=True):
+    """Build the model."""
     print_rank_0('Building GPT model ...')
     config = core_transformer_config_from_args(get_args())
     model = GPTModel(
         config,
         num_tokentypes=0,
         parallel_output=True,
-        pre_process=True,
-        post_process=True
+        pre_process=pre_process,
+        post_process=post_process
     )
+    return model
 
+
+def dataset_builder(train_val_test_num_samples):
+    """Build the dataset."""
     args = get_args()
     print_rank_0('Load GPT dataset ...')
-    # Number of train/valid/test samples.
-    if args.train_samples:
-        train_samples = args.train_samples
-    else:
-        train_samples = args.train_iters * args.global_batch_size
-    eval_iters = (args.train_iters // args.eval_interval + 1) * \
-        args.eval_iters
-    test_iters = args.eval_iters
-    train_val_test_num_samples = [train_samples,
-                                  eval_iters * args.global_batch_size,
-                                  test_iters * args.global_batch_size]
-    print_rank_0(' > datasets target sizes (minimum size):')
-    print_rank_0('    train:      {}'.format(train_val_test_num_samples[0]))
-    print_rank_0('    validation: {}'.format(train_val_test_num_samples[1]))
-    print_rank_0('    test:       {}'.format(train_val_test_num_samples[2]))
     train_ds, valid_ds, test_ds = build_train_valid_test_datasets(
         data_prefix=args.data_path,
         data_impl=args.data_impl,
@@ -131,9 +53,62 @@ def main():
         valid_data_prefix=args.valid_data_path,
         test_data_prefix=args.test_data_path,
         data_cache_path=args.data_cache_path)
-    
-    pretrain(model, train_ds, valid_ds, test_ds)
+    return train_ds, valid_ds, test_ds
+
+
+def forward_step(data_iterator, model):
+    """Forward step."""
+    timers = get_timers()
+    timers('batch-generator', log_level=2).start()
+    tokens, labels, loss_mask, attention_mask, position_ids = get_batch(
+        data_iterator)
+    timers('batch-generator').stop()
+    output_tensor = model(tokens, position_ids, attention_mask, labels=labels)
+
+    return output_tensor, partial(loss_func, loss_mask)
+
+
+def get_batch(data_iterator):
+    """Generate a batch."""
+    args = get_args()
+    tokenizer = get_tokenizer()
+
+    # Items and their type.
+    keys = ['text']
+    datatype = torch.int64
+
+    # Broadcast data.
+    if data_iterator is not None:
+        data = next(data_iterator)
+    else:
+        data = None
+    data_b = tensor_parallel.broadcast_data(keys, data, datatype)
+
+    # Unpack.
+    tokens_ = data_b['text'].long()
+    labels = tokens_[:, 1:].contiguous()
+    tokens = tokens_[:, :-1].contiguous()
+
+    # Get the masks and postition ids.
+    attention_mask, loss_mask, position_ids = get_ltor_masks_and_position_ids(
+        tokens,
+        tokenizer.eod,
+        args.reset_position_ids,
+        args.reset_attention_mask,
+        args.eod_mask_loss)
+
+    return tokens, labels, loss_mask, attention_mask, position_ids
+
+
+def loss_func(loss_mask, output_tensor):
+    """Loss function."""
+    raw_loss = output_tensor.view(-1).float()
+    loss_mask = loss_mask.view(-1).float()
+    loss = torch.sum(raw_loss * loss_mask) / loss_mask.sum()
+    # Reduce loss for logging.
+    averaged_loss = average_losses_across_data_parallel_group([loss])
+    return loss, {'lm loss': averaged_loss[0]}
 
 
 if __name__ == "__main__":
-    main()
+    pretrain(model_builder, dataset_builder, forward_step)
--- training/DeepSpeed-Domino/pretrain_gpt3_6.7b.sh
+++ training/DeepSpeed-Domino/pretrain_gpt3_6.7b.sh
@@ -1,24 +1,24 @@
-# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
 # This file is adapted from pretrain_gpt.sh in Megatron-LM
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
 
 #!/bin/bash --login
 
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-GPUS_PER_NODE=2
+GPUS_PER_NODE=4
 MASTER_ADDR=localhost
 MASTER_PORT=6001
 NNODES=1
 NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
  
-CHECKPOINT_PATH=checkpoint
-rm -rf $CHECKPOINT_PATH/*
+
 VOCAB_FILE="dataset/gpt2-vocab.json"
 MERGE_FILE="dataset/gpt2-merges.txt"
-DATA_PATH="dataset/my-gpt2_text_document"
+DATA_PATH="dataset/BookCorpusDataset_text_document"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$SCRIPT_DIR/Megatron-LM"
 export PYTHONPATH=$SCRIPT_DIR:$PYTHONPATH
 
 DISTRIBUTED_ARGS="
@@ -33,12 +33,12 @@ GPT_ARGS="
     --num-layers 32 \
     --hidden-size 4096 \
     --num-attention-heads 32 \
-    --seq-length 2048 \
-    --max-position-embeddings 2048 \
+    --seq-length 1024 \
+    --max-position-embeddings 1024 \
     --micro-batch-size 8 \
     --global-batch-size 8 \
     --lr 0.00015 \
-    --train-iters 80 \
+    --train-iters 100 \
     --lr-decay-iters 320000 \
     --lr-decay-style cosine \
     --min-lr 1.0e-5 \
@@ -46,6 +46,7 @@ GPT_ARGS="
     --lr-warmup-fraction .01 \
     --clip-grad 1.0 \
     --fp16 \
+    --no-gradient-accumulation-fusion \
     --tensor-model-parallel-size $WORLD_SIZE
 "
 
@@ -58,6 +59,7 @@ DATA_ARGS="
  
 OUTPUT_ARGS="
     --log-interval 1 \
+    --eval-iters 1
 "
 
 cmd="deepspeed --num_gpus $WORLD_SIZE \
@@ -66,5 +68,5 @@ cmd="deepspeed --num_gpus $WORLD_SIZE \
     $DATA_ARGS \
     $OUTPUT_ARGS 
     " 
-echo $cmd
+# echo $cmd
 eval $cmd 
